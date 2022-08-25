f0a57c582bc5fccdf91b09197922da9f53c3991c
vagrant <jeandrev1414@gmail.com>
2022-08-25 23:54:19 +0000

    Add method to look up user if they are logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,19 @@ class ApplicationController < ActionController::Base
   
   protect_from_forgery with: :exception
 
+  # look up the user, if they're logged in, and save their user object to @current_user
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  # helper_method allows for us to use in our view files
+  helper_method :current_user
+
+  # Authorize is for sending someone to the login page if they aren't logged in
+  def authorize
+    redirect_to '/login' unless current_user
+  end
+
   private
 
   def cart
