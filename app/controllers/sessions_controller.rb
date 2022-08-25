c84a068595b25896383df295e5ed54ddb52383e3
vagrant <jeandrev1414@gmail.com>
2022-08-25 23:47:18 +0000

    Add logic to create and destroy actions to log a user in and out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,21 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by_email(params[:email])
+    # If the user exists AND the password entered is correct.
+    if @user && @user.authenticate(params[:password])
+      # Save the user id inside the browser cookie. This is how we keep the user logged in when they navigate around our website.
+      session[:user_id] = @user.id
+      redirect_to :root
+    else
+      render :new
+    end
   end
 
+  # remove cookie
   def destroy
+    session[:user_id] = nil
+    redirect_to :root
   end
 
 end
