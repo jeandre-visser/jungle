8b06b8132ebdc7974478259a2d7d7c5c3a223fbb
vagrant <jeandrev1414@gmail.com>
2022-08-25 03:09:05 +0000

    Replace name and password values with references to the ENV admin username and password

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,6 +1,6 @@
 class Admin::ProductsController < ApplicationController
-  
-  http_basic_authenticate_with name: 'username', password: 'password'
+
+  http_basic_authenticate_with ENV["ADMIN_USERNAME"], password: ENV["ADMIN_PASSWORD"]
 
   def index
     @products = Product.order(id: :desc).all
