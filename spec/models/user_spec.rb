87268c6fb6d4b0d390a1442ddbfea9aa2d79bd70
vagrant <jeandrev1414@gmail.com>
2022-08-29 21:49:29 +0000

    Test for user not valid without first name

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,6 +8,14 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages).to be_empty
     end
 
+
+    it "is not valid without a first name" do
+      @user = User.new(first_name: nil, last_name: 'Visser', email: 'visser@test.com', password: 'tester', password_confirmation: 'tester')
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to include "First name can't be blank"
+    end
+    
+
   end
 
   describe '.authenticate_with_credentials' do
