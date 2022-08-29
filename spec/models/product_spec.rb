6958247e07ddd602e8d7df4bd5586db6b02148b6
vagrant <jeandrev1414@gmail.com>
2022-08-29 20:26:56 +0000

    Test for a valid product with valid name, price, quantity, and category

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-    # validation tests/examples here
+    it "is valid with a name, price, quantity, and category" do
+      @category = Category.new(name: 'Trees')
+      @product = Product.new(name: 'Spruce Tree', price_cents: 1000, quantity: 1, category: @category)
+      expect(@product).to be_valid
+    end
   end
 end
