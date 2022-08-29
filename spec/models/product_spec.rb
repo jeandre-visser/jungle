03c8f6a4d853969be8d78b74f1450ba97d9b8b93
vagrant <jeandrev1414@gmail.com>
2022-08-29 20:36:38 +0000

    Test that the expect error is found within the .errors.full_messages array

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,12 +6,14 @@ RSpec.describe Product, type: :model do
       @category = Category.new(name: 'Trees')
       @product = Product.new(name: 'Spruce Tree', price_cents: 1000, quantity: 1, category: @category)
       expect(@product).to be_valid
+      expect(@product.errors.full_messages).to be_empty
     end
 
     it "is not valid without a name" do
       @category = Category.new(name: 'Trees')
       @product = Product.new(name: nil, price_cents: 1000, quantity: 1, category: @category)
       expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include "Name can't be blank"
     end
   end
 end
