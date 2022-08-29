8cd6218479e1dcc408dc7d20f3f254c3dda5f747
vagrant <jeandrev1414@gmail.com>
2022-08-29 20:39:33 +0000

    Test for product not valid when there is no quantity

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -22,5 +22,12 @@ RSpec.describe Product, type: :model do
       expect(@product).to_not be_valid
       expect(@product.errors.full_messages).to include "Price can't be blank"
     end
+
+    it "is not valid without a quantity" do
+      @category = Category.new(name: 'Trees')
+      @product = Product.new(name: 'Spruce Tree', price_cents: 1000, quantity: nil, category: @category)
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include "Quantity can't be blank"
+    end
   end
 end
