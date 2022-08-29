37bfc989f1c0e3ca33ea40c2ebf6b2ad3d041831
vagrant <jeandrev1414@gmail.com>
2022-08-29 21:07:01 +0000

    Test for user with valid attributes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  
+  describe 'Validations' do
+    it "is valid when all user attributes are valid" do
+      @user = User.new(first_name: 'Jeandre', last_name: 'Visser', email: 'visser@test.com', password: 'tester', password_confirmation: 'tester')
+      expect(@user).to be_valid
+      expect(@user.errors.full_messages).to be_empty
+    end
+
+  end
+
+  describe '.authenticate_with_credentials' do
+    # examples for this class method here
+  end
 end
