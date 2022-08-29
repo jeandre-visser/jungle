1718bf0331bc8c1f55e47bf786dcc4ad95349707
vagrant <jeandrev1414@gmail.com>
2022-08-29 22:34:41 +0000

    Test for user not valid without fira last name, email, password, or password confirmation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,6 +15,29 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages).to include "First name can't be blank"
     end
     
+    it "is not valid without a last name" do
+      @user = User.new(first_name: 'Jeandre', last_name: nil, email: 'visser@test.com', password: 'tester', password_confirmation: 'tester')
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to include "Last name can't be blank"
+    end
+
+    it "is not valid without an email" do
+      @user = User.new(first_name: 'Jeandre', last_name: 'Visser', email: nil, password: 'tester', password_confirmation: 'tester')
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to include "Email can't be blank"
+    end
+
+    it "is not valid without a password" do
+      @user = User.new(first_name: 'Jeandre', last_name: 'Visser', email: 'visser@test.com', password: nil)
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to include "Password can't be blank"
+    end
+
+    it "is not valid without password_confirmation" do
+      @user = User.new(first_name: 'Jeandre', last_name: 'Visser', email: nil, password: 'tester', password_confirmation: nil)
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to include "Password confirmation can't be blank"
+    end
 
   end
 
