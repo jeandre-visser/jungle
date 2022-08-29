ebca93ea18816e8f8eb0ced7a993a5c7e4d403d5
vagrant <jeandrev1414@gmail.com>
2022-08-29 20:40:54 +0000

    Test for product not valid when there is no category

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -29,5 +29,12 @@ RSpec.describe Product, type: :model do
       expect(@product).to_not be_valid
       expect(@product.errors.full_messages).to include "Quantity can't be blank"
     end
+
+    it "is not valid without a category" do
+      @category = Category.new(name: 'Trees')
+      @product = Product.new(name: 'Spruce Tree', price_cents: 1000, quantity: 1, category: nil)
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include "Category can't be blank"
+    end
   end
 end
