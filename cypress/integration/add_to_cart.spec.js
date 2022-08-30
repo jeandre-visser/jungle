a34eae64879369bf527cdeceacfcc30214411a4c
vagrant <jeandrev1414@gmail.com>
2022-08-30 23:10:40 +0000

    Remove spaces

--- cypress/integration/add_to_cart.spec.js
+++ cypress/integration/add_to_cart.spec.js
@@ -2,10 +2,8 @@ describe('Jungle', () => {
   beforeEach(() => {
     cy.visit('/')
   })
-
   it("A product can be added to the cart from the homepage", () => {
     cy.get("article:first form button").click({ force: true });
     cy.get("nav").should("contain", "My Cart (1)");
   });
-
 })
